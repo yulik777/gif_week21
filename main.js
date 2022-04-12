8a117905b5a4dba892da4561bf8a4eb664c076c6
Yuliia Skabitskaya <91159859+yulik777@users.noreply.github.com>
2022-04-12 15:38:25 +0300

    first_commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function onSearch() {
+    let gif = document.getElementById("gif").value;
+    console.log(gif);
+
+    fetch('https://api.giphy.com/v1/gifs/search?api_key=Qvf9S52TwRjgZhRgCtYHB9Xm1bMAjTj4&q=' + gif +
+            '&limit=5&offset=0&rating=g&lang=e')
+        .then(response => response.json())
+        .then(gif => {
+            console.log(gif);
+            gif.data.forEach(element => {
+                console.log(element);
+                const box = document.createElement("img");
+                box.classList.add('box');
+                box.src = element.images.original.url;
+                pictures.append(box);
+            });
+        })
+
+        .catch(error => console.log(error));
+}
